2b06b77ef1fa55b512cef7593962281f46c834ff
DeDomosed <poperm137@gmail.com>
2026-09-15 10:08:20 +0300

    lar7

---
+++ labs/ЛР7/LabWork7/task4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+var sourceSwitch = new SourceSwitch("MyAppSwitch")
+{
+    Level = SourceLevels.Warning
+};
+
+var traceSource = new TraceSource("MyApp")
+{
+    Switch = sourceSwitch
+};
+
+var sourceSwitchStorage = new SourceSwitch("StorageSwitch")
+{
+    Level = SourceLevels.Warning
+};
+
+var traceStorage = new TraceSource("Storage")
+{
+    Switch = sourceSwitch
+};
+
+traceSource.Listeners.Clear();
+traceSource.Listeners.Add(new TextWriterTraceListener("trace.log", "fileListener"));
+traceSource.Listeners.Add(new ConsoleTraceListener());
+
+traceSource.Listeners.Clear();
+traceSource.Listeners.Add(new TextWriterTraceListener("storage.log", "fileListener"));
+
